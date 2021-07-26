b31aa367d111caec08229e1aff9156efa0325c2d
Mucahit Ozkanca <mmucahit17@hotmail.com>
2021-07-26 19:44:38 +0300

    express app added

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+// like import
+const express = require('express');
+const app = express(); // create express app
+
+app.listen(3000, () => console.log( "listening 3000" ) );
+// create folder 'public'
+app.use( express.static('public') );
+
