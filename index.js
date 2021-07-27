d31d6c8f3da3be8a848205df499b9c04e1aef802
Mucahit Ozkanca <mmucahit17@hotmail.com>
2021-07-28 00:49:57 +0300

    made a post to /api

--- index.js
+++ index.js
@@ -9,4 +9,19 @@ const app = express(); // create express app
 app.listen(3000, () => console.log( "listening 3000" ) );
 // create folder 'public'
 app.use( express.static('public') );
+app.use( express.json( { limit:'1mb' } ) );
 
+app.post( '/api', (request, response) => {
+    
+    console.log( request.body );
+    const data = request.body;
+
+    response.json( {
+        status:'success',
+        latitude: data.lat,
+        longitude: data.lon
+    } );
+
+    response.end();
+
+} );
