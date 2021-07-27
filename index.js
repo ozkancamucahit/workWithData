ff9b4950826d0ad7345aeeeea865b36f31e7adb4
Mucahit Ozkanca <mmucahit17@hotmail.com>
2021-07-27 13:06:58 +0300

    two pages added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
+//this is the server
+
 
 // like import
-const express = require('express');
+const express = require('express'); 
 const app = express(); // create express app
 
 app.listen(3000, () => console.log( "listening 3000" ) );
