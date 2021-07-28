44f45aa0a0b0607cf3313428bd4f9a0e370ff30a
Mucahit Ozkanca <mmucahit17@hotmail.com>
2021-07-28 21:43:54 +0300

    added image logs

--- public/logs/logs.js
+++ public/logs/logs.js
@@ -2,10 +2,12 @@
 getData();
 
 async function getData() {
+
   const response = await fetch('/api');
   const data = await response.json();
 
   for (item of data) {
+    
     const root = document.createElement('p');
     const geo = document.createElement('div');
     const date = document.createElement('div');
