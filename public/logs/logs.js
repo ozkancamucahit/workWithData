9b84e1f7033479c449147e8f1756a43f33980551
Mucahit Ozkanca <mmucahit17@hotmail.com>
2021-07-29 22:40:42 +0300

    added markers for checkins

--- public/logs/logs.js
+++ public/logs/logs.js
@@ -1,10 +1,17 @@
 
+
+const mymap = L.map("checkinMap").setView([0, 0], 1);
+const attribution ='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer( tileUrl, {attribution} );
+tiles.addTo(mymap);
 async function getData() {
 
   const response = await fetch('/api');
   const data = await response.json();
 
-  // for (item of data) {
+  for (item of data) {
+    L.marker( [ item.lat, item.lon ] ).addTo( mymap );
     
   //   const root = document.createElement('p');
   //   const geo = document.createElement('div');
@@ -16,24 +23,10 @@ async function getData() {
 
   //   root.append( geo, date );
   //   document.body.append(root);
-  // }
+   }
   console.log(data);
 }
 
-const mymap = L.map('issMap').setView([0, 0], 1);
-        const attribution ='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
 
-        const myIcon = L.icon({
-        iconUrl: 'iss200.png',
-        iconSize: [50, 30],
-        iconAnchor: [25, 15],
-        popupAnchor: [-3, -76]
-        });
-        const marker = L.marker( [0,0], {icon:myIcon} ).addTo(mymap);
-
-
-        const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-        const tiles = L.tileLayer( tileUrl, {attribution} );
-        tiles.addTo(mymap);
 
 getData();
